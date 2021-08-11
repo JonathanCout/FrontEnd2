51a8e4c0986eb3f4ccd4a395baf191b1a2299fff
JonathanCout <jqcoutinho.eng@gmail.com>
2021-08-11 12:48:14 -0300

    Jogo pedra, papel, tesoura

---
+++ Aula02/script/app.js
@@ -0,0 +1,69 @@
+// Criando a variável de confirmação do jogo
+let lilGame = confirm("Vamos brincar de pedra, papel ou tesoura?")
+
+if (lilGame) {
+    let userPoints = 0
+    let pcPoints = 0
+/*  Loop até um dos jogadores conseguirem 3 pontos. Foi feita desta forma pois em 3 rodadas, pode-se não ter um
+resultado definitivo, pois podem existir 3 empates
+ */    while(userPoints < 3 && pcPoints < 3) {
+        // Array de possibilidades
+        let choices = ["pedra","papel","tesoura"]
+        // Escolha feita pelo pc atribuindo um valor aleatório entre 0, 1 e 2 e atribuindo ao array choices
+        let pcChoice = choices[Math.floor(Math.random()*choices.length)]
+        let userChoice = prompt("Escolha entre Pedra, Papel ou Tesoura")
+        let userChoiceConfirm = choices.find(choice => choice === userChoice.toLowerCase())
+        if (!userChoiceConfirm) {
+            alert("Escolha uma opção válida")
+        } else switch (pcChoice + userChoiceConfirm) {
+            case "pedrapedra":
+            case "tesouratesoura":
+            case "papelpapel":
+                alert(`Sua escolha: ${userChoiceConfirm}. Minha escolha: ${pcChoice}. Êpa, nós empatamos. Vamos de novo`) 
+            break;
+            case "pedratesoura":
+            case "tesourapapel":
+            case "papelpedra":
+                pcPoints++
+                alert(`Sua escolha: ${userChoiceConfirm}. Minha escolha: ${pcChoice}. Eu ganhei essa! =P. 
+                Sua pontuação: ${userPoints}. Minha pontuação: ${pcPoints}`)
+            break;
+            default:         
+                userPoints++
+                alert(`Sua escolha: ${userChoiceConfirm}. Minha escolha: ${pcChoice}. Droga, você ganhou essa. 
+                Sua pontuação: ${userPoints}. Minha pontuação: ${pcPoints}`)
+        }
+    } 
+    if (userPoints == 3) {
+        alert("Ok... dessa vez você ganhou")
+    } else if (pcPoints == 3) {
+        alert("HAHA! Dessa vez eu ganhei!")
+    }
+    
+} else alert("Poxa que pena")
+
+
+
+
+
+
+
+/*   else if (userChoiceConfirm==pcChoice) {
+      alert(`Sua escolha: ${userChoiceConfirm}. Minha escolha: ${pcChoice}. Êpa, nós empatamos. Vamos de novo`)
+  } else if (userChoiceConfirm=="pedra" && pcChoice == "papel") {
+     pcPoints++
+     alert(`Sua escolha: ${userChoiceConfirm}. Minha escolha: ${pcChoice}. Eu ganhei essa! =P. 
+     Sua pontuação: ${userPoints}. Minha pontuação: ${pcPoints}`)
+  } else if (userChoiceConfirm=="papel" && pcChoice == "tesoura") {
+     pcPoints++
+     alert(`Sua escolha: ${userChoiceConfirm}. Minha escolha: ${pcChoice}. Eu ganhei essa! =P. 
+     Sua pontuação: ${userPoints}. Minha pontuação: ${pcPoints}`)
+  } else if (userChoiceConfirm=="tesoura" && pcChoice == "pedra") {
+     pcPoints++
+     alert(`Sua escolha: ${userChoiceConfirm}. Minha escolha: ${pcChoice}. Eu ganhei essa! =P. 
+     Sua pontuação: ${userPoints}. Minha pontuação: ${pcPoints}`)
+  } else {
+      userPoints++
+      alert(`Sua escolha: ${userChoiceConfirm}. Minha escolha: ${pcChoice}. Droga, você ganhou essa. 
+      Sua pontuação: ${userPoints}. Minha pontuação: ${pcPoints}`)
+  }    */
