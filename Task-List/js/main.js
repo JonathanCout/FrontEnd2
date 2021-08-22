f7593db58c09d57f3398c591a2fd52dea50bebf1
JonathanCout <jqcoutinho.eng@gmail.com>
2021-08-22 10:00:22 -0300

    added check button and delete button (need fix)

--- Task-List/js/main.js
+++ Task-List/js/main.js
@@ -1,19 +1,79 @@
-const root = document.querySelector(':root')
 const list = document.querySelector('.lista')
 const form = document.querySelector('.form')
-const formbutton = document.querySelector('.form-botao')
+const formButton = document.querySelector('.form-botao')
+const task = document.querySelector('.tarefa')
+const deleteButton = document.querySelector('.delete-task')
 let taskCounter = 0
 const tasks = []
 
+
+
+// Capturação da tarefa e criação de "li"
 const showText = () => {
     const newLi = document.createElement("li")
-    newLi.id = `tarefa${taskCounter + 2}`
+    const input = document.createElement('input')
+    const label = document.createElement('label')
+    const button = document.createElement('button') 
+
+    const inputModel = [
+        {
+            key: "type",
+            value: "checkbox"
+        },
+        {
+            key: "name",
+            value: `task${taskCounter+2}`
+        },
+        {
+            key: "id",
+            value: `task${taskCounter+2}`
+        }
+    ]
+    const labelModel = [
+        {
+            key: "for",
+            value: `task${taskCounter+2}`
+        }
+    ]
+    const buttonModel = [
+        {
+            key: "type",
+            value: "button"
+        },
+        {
+            key: "class",
+            value: "delete-task"
+        }
+    ]
+
     newLi.classList.add('tarefa') 
-    newLi.innerHTML = tasks[taskCounter]
-    document.getElementById(`tarefa${taskCounter + 1}`).after(newLi)
+    newLi.id = `tarefa${taskCounter + 2}`
+    list.appendChild(newLi)
+
+    for (let i of inputModel) {
+        input.setAttribute(i.key,i.value)
+    }
+    for (let l of labelModel) {
+        label.setAttribute(l.key,l.value)
+    }
+    for (let b of buttonModel) {
+        button.setAttribute(b.key,b.value)
+    }
+    
+    newLi.appendChild(input)
+    newLi.appendChild(label)
+    newLi.appendChild(button)
+
+    if(taskCounter < 0) {
+        label.innerHTML = tasks[0]  
+    } else {
+        label.innerHTML = tasks[taskCounter]  
+    }
+    
     taskCounter++
 }
 
+// Inserção de dados
 const listSetter = () => {
     let newTask = document.querySelector('.form-input').value
     if (newTask.length == 0) {
@@ -25,13 +85,19 @@ const listSetter = () => {
     document.querySelector('.form-input').value = ''
 }
 
+// Deletar tarefas e setar taskCounter
+const deleteTask = () => {
+    task.parentNode.removeChild(task)
+}
+
+
 // Impedir que a página seja recarregada ao apertar o botão 'nova tarefa'
 const prevent = (event) => {
     event.preventDefault()
 }
 form.addEventListener('submit',prevent)
 
-formbutton.addEventListener('click',listSetter)
+formButton.addEventListener('click',listSetter)
 
-
+deleteButton.addEventListener('click',deleteTask)
 
