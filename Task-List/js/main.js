11595dde5cb100771e80faea3de2c8f5c0fe0c56
JonathanCout <jqcoutinho.eng@gmail.com>
2021-08-22 19:28:06 -0300

    delete button still bugged (fml)

--- Task-List/js/main.js
+++ Task-List/js/main.js
@@ -2,7 +2,6 @@ const list = document.querySelector('.lista')
 const form = document.querySelector('.form')
 const formButton = document.querySelector('.form-botao')
 const task = document.querySelector('.tarefa')
-const deleteButton = document.querySelector('.delete-task')
 let taskCounter = 0
 const tasks = []
 
@@ -11,10 +10,12 @@ const tasks = []
 // Capturação da tarefa e criação de "li"
 const showText = () => {
     const newLi = document.createElement("li")
+    // Atributos da nova "li"
     const input = document.createElement('input')
     const label = document.createElement('label')
-    const button = document.createElement('button') 
+    const button = document.createElement('button')
 
+    // Modelos dos atributos
     const inputModel = [
         {
             key: "type",
@@ -22,54 +23,56 @@ const showText = () => {
         },
         {
             key: "name",
-            value: `task${taskCounter+2}`
+            value: `task${taskCounter + 2}`
         },
         {
             key: "id",
-            value: `task${taskCounter+2}`
+            value: `task${taskCounter + 2}`
         }
     ]
     const labelModel = [
         {
             key: "for",
-            value: `task${taskCounter+2}`
+            value: `task${taskCounter + 2}`
+        },
+        {
+            key: "id",
+            value: `label${taskCounter + 2}`
         }
     ]
     const buttonModel = [
         {
             key: "type",
             value: "button"
-        },
-        {
-            key: "class",
-            value: "delete-task"
         }
     ]
-
-    newLi.classList.add('tarefa') 
+    // Inserção dos dados nas "li"
+    newLi.classList.add('tarefa')
     newLi.id = `tarefa${taskCounter + 2}`
     list.appendChild(newLi)
 
     for (let i of inputModel) {
-        input.setAttribute(i.key,i.value)
+        input.setAttribute(i.key, i.value)
     }
     for (let l of labelModel) {
-        label.setAttribute(l.key,l.value)
+        label.setAttribute(l.key, l.value)
     }
     for (let b of buttonModel) {
-        button.setAttribute(b.key,b.value)
+        button.setAttribute(b.key, b.value)
     }
-    
+    // Criando nós entre "li" e seus filhos
     newLi.appendChild(input)
     newLi.appendChild(label)
+    button.classList.add('delete-task')
     newLi.appendChild(button)
 
-    if(taskCounter < 0) {
-        label.innerHTML = tasks[0]  
+    // Se não existir tarefas, ele oferece o valor inicial
+    if (taskCounter < 0) {
+        label.innerHTML = tasks[0]
     } else {
-        label.innerHTML = tasks[taskCounter]  
+        label.innerHTML = tasks[taskCounter]
     }
-    
+
     taskCounter++
 }
 
@@ -79,13 +82,13 @@ const listSetter = () => {
     if (newTask.length == 0) {
         alert("Favor inserir alguma tarefa")
         return
-    } 
-    tasks.push(newTask) 
+    }
+    tasks.push(newTask)  
     showText()
     document.querySelector('.form-input').value = ''
 }
 
-// Deletar tarefas e setar taskCounter
+// Deletar tarefas 
 const deleteTask = () => {
     task.parentNode.removeChild(task)
 }
@@ -95,9 +98,12 @@ const deleteTask = () => {
 const prevent = (event) => {
     event.preventDefault()
 }
-form.addEventListener('submit',prevent)
+form.addEventListener('submit', prevent)
 
-formButton.addEventListener('click',listSetter)
+// Botão de inserção
+formButton.addEventListener('click', listSetter)
 
-deleteButton.addEventListener('click',deleteTask)
+// Botão de deletar
+const deleteButton = document.querySelectorAll('.delete-task')
+deleteButton.forEach(btn => btn.addEventListener('click', deleteTask))
 
