bb095aa6c4564bc33cc85f4923be2f022d6c3efd
JonathanCout <jqcoutinho.eng@gmail.com>
2021-08-25 12:09:25 -0300

    commiting past exercises

---
+++ Aula03/calc.js
@@ -0,0 +1,6 @@
+let soma = require('./modules/somar')
+let subtracao = require ('./modules/subtrair')
+let divisao = require('./modules/dividr')
+let multiplicacao = require ('./modules/multiplicar')
+
+ 
---
+++ Aula03/modules/dividr.js
@@ -0,0 +1,7 @@
+let dividir = (a,b) => {
+    if (b == 0) {
+        return 'Impossível divisão por 0'
+    } else return a / b
+}
+
+module.exports = dividir
